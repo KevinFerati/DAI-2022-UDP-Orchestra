1d0b0425143dcef61ad4584cbb6d7c5e34ff42a0
Greywind299792458 <71220948+Greywind299792458@users.noreply.github.com>
2023-01-25 13:22:57 +0100

    removed some comments and added some order

--- docker/image-auditor/src/index.js
+++ docker/image-auditor/src/index.js
@@ -1,14 +1,14 @@
 // change to '../../config.json' to test locally ---- FIXME WITH BETTER SOLUTION
-const mainConfig = require('./config.json');
+const mainConfig    = require('./config.json');
 const auditorConfig = require('./auditor_config.json');
-const net = require('net');
+const net   = require('net');
 const dgram = require('dgram');
 
 let orchestra = new Map();
 
 class Musician {
     constructor(uuid, sound) {
-      this.uuid = uuid;
+      this.uuid       = uuid;
       this.instrument = ""
       // looking for the instrument making the given sound
       for (const [key, value] of Object.entries(mainConfig.instruments)) {
@@ -16,27 +16,33 @@ class Musician {
             this.instrument = key;
             break;
         }
-      } // setting up the date
+      }
       this.activeSince = new Date();
     }
 }
+
+// ============================ Musicians management functions ==================================== //
 
 function registerMusicianSound(uuid, sound) {
   orchestra.set(uuid, new Musician(uuid, sound));
 }
 
+function removeInactivePlayers() { 
+  for (let [uuid, musician] of orchestra) {
+    const secondsSinceLastSound = ((new Date()).getTime() - musician.activeSince.getTime()) / 1000;
+    if (secondsSinceLastSound > auditorConfig.maxSecondsAsInactive) {
+      // musician is inactive => we remove it from the orchestra following its unique uuid
+      orchestra.delete(uuid);
+    }
+  }
+}
+
+// =============================== main Function =================================================== //
+
 // to be deleted - debugging purpose
 function test() {
   registerMusicianSound(123456, "pouet");
   setInterval(function() {registerMusicianSound(123, "trulu");}, 10000);
-}
-
-function removeInactivePlayers() { 
-  for (let [uuid, musician] of orchestra) {
-    const seconds = ((new Date()).getTime() - musician.activeSince.getTime()) / 1000;
-    if (seconds > auditorConfig.MaxSecondsAsInactive)
-      orchestra.delete(uuid);
-  }
 }
 
 function init() {
@@ -45,14 +51,16 @@ function init() {
   // start sending informations about the musicians
   startTCPStream();
 
-  // image musician mock test - to be deleted - debugging purpose
-  // test();
+  // to be deleted - debugging purpose
+  test();
 
-  // detect and remove inactive musicians every 5 seconds
-  setInterval(removeInactivePlayers, 5000);
+  // detect and remove inactive musicians every x seconds
+  // X = number of seconds without playing to be considered inactive
+  setInterval(removeInactivePlayers, (auditorConfig.maxSecondsAsInactive * 1000));
 }
 
-// UDP subscription to the orchestra
+// =============================== UDP subscription to the orchestra =============================== //
+
 function startUDPSubscribtion() {
   const socket = dgram.createSocket('udp4');
   socket.bind(mainConfig.musicianToListenerProtocol.port, function() {
@@ -66,7 +74,8 @@ function startUDPSubscribtion() {
   });
 }
 
-// start TCP server to publish the orchestra info
+// =============================== TCP server to publish the orchestra info ======================= //
+
 function startTCPStream() {
   const server = new net.Server();
   server.listen(auditorConfig.TCPServerPort, function() {
@@ -75,10 +84,8 @@ function startTCPStream() {
 
   server.on('connection', function(socket) {
     console.log('TCP --------------- a client is connected !');
-    // when a client is connected : directly send the orchestra info
+    // directly send the orchestra info when a client is connected
     socket.write(JSON.stringify(Array.from(orchestra.entries())) + "\n");
-    
-    // emit the data only once - Half-close the socket
     socket.end();
 
     socket.on('end', function() {
